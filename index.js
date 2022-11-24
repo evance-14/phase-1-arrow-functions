42114d2a6ad175dc8e21f06bac9ec02211923846
evance-14 <kiropevans097@gmail.com>
2022-11-24 18:23:17 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,12 @@
+const divide =(parameter1, parameter2) =>{
+    console.log('divide ${parameter1}');
+    console.log('divide ${parameter2}');
+    return 2000/100;
+}
+const square = num1 =>num1 * num1;
+
+
+const add=(a,b) =>a + b;
+
+    
+    
